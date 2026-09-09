525d7106d34d371d8459116f1637b347a5ae45f6
Tarun <tarunksm.07@gmail.com>
2026-09-09 21:10:28 +0530

    Correct validation

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -5,7 +5,7 @@ export const validate = (schema: z.ZodSchema) => {
   return (req: Request, _res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
     if (!result.success) {
-      next(result.error);
+      return next(result.error);
     }
     next();
   };
